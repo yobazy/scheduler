68b09d5cbecb7a6aba498d1e1b950b85f0f0091d
Bazil <king.bazil.khan@gmail.com>
2022-06-17 15:00:05 -0600

    Adds DayList container for DayListItems

---
+++ src/components/DayList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import DayListItem from "./DayListItem.js"
+
+
+export default function DayList(props) {
+  const listDays = props.days.map((day) => {
+  <DayListItem         
+    key={day.id}
+    name={day.name} 
+    spots={day.spots} 
+    selected={day.name === props.day}
+    setDay={props.setDay}  
+  />;
+  });
+  return <ul>{listDays}</ul>;
+};
