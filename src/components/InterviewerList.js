cc712f70cf7330a3da5e08f9b137441d9258b43b
Bazil <king.bazil.khan@gmail.com>
2022-06-17 17:23:51 -0600

    Adds interviewer list states

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,10 +1,27 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem.js"
+import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
-  // const listInterviewers = props.days.map((day) => {
+  // const listInterviewers = props.interviewers.map((day) => {
   // return <InterviewerListItem
-  // id=int.id         />
+  // id=
+  // name=
+  // avatar=
+  // />
   // });
   // return <ul>{listInterviewers}</ul>;
+  const listInterviewers = props.interviewers.map((interviewer) => {
+  return <InterviewerListItem
+    key={interviewer.id}
+    name={interviewer.name}
+    avatar={interviewer.avatar}
+    selected={interviewer.selected}
+    />
+  });
+  return (
+  <section className="interviewers">
+  <h4 className="interviewers__header text--light">Interviewer</h4>
+  <ul className="interviewers__list">{listInterviewers}</ul>
+</section>)
 };
