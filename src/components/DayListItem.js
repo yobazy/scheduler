8d6c06451ad5c01d9505df4c6d98e605127fefa3
Bazil <king.bazil.khan@gmail.com>
2022-06-17 14:35:43 -0600

    Updates DayListItem to be condition based on spots available

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,6 +2,15 @@ import React from "react";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  const formatSpots = function(spots)  {
+    if (spots === 0){
+      return "no spots";
+    };
+    if (spots === 1){
+      return "1 spot";
+    };
+    return spots+" spots";
+  };
   let dayClass = "day-list__item";
   if (props.selected) {
     dayClass += "--selected";
@@ -13,7 +22,7 @@ export default function DayListItem(props) {
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)} remaining</h3>
     </li>
   );
 }
