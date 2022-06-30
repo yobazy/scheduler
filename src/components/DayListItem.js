396a3e3959a75919cffc90d74eb2f6e02b4ddd9f
Bazil <king.bazil.khan@gmail.com>
2022-06-30 13:32:11 -0600

    Updates DayListItem component for testing

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,37 +1,35 @@
 import React from "react";
 import "components/DayListItem.scss";
-import classNames from "classnames";
+const classNames = require('classnames');
+
+const formatSpots = spots => {
+  if (!spots) {
+    return `no spots remaining`;
+  }
+
+  if (spots === 1) {
+    return `${spots} spot remaining`;
+  }
+
+  return `${spots} spots remaining`;
+}
 
 export default function DayListItem(props) {
-  
-  const formatSpots = function(spots)  {
-    if (spots === 0){
-      return "no spots";
-    };
-    if (spots === 1){
-      return "1 spot";
-    };
-    return spots+" spots";
-  };
+  const availabilityMessage = formatSpots(props.spots);
 
-  const dayListItemClass = classNames("day-list__item",{
-    "day-list__item--selected": props.selected,
-    "day-list__item--full": props.spots === 0
+  let dayClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': !props.spots
   })
 
-  // let dayClass = "day-list__item";
-  // if (props.selected) {
-  //   dayClass += "--selected";
-  // }
-  // if (props.spots === 0) {
-  //   dayClass += "--full";
-  // }
-
-
   return (
-    <li className={dayListItemClass} onClick={() => props.setDay(props.name)}>
+    <li 
+      className={dayClass}
+      onClick={() => props.setDay(props.name)}
+      data-testid="day"
+    >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{formatSpots(props.spots)} remaining</h3>
+      <h3 className="text--light">{availabilityMessage}</h3>
     </li>
   );
 }
