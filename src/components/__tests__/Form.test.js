f84c182b6698261ed4ed58eca15c3c1bf8002ddf
Bazil <king.bazil.khan@gmail.com>
2022-06-29 18:31:29 -0600

    Fixes tests for form component

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -1,17 +1,12 @@
 import React from "react";
 
 import { render, cleanup } from "@testing-library/react";
-import { fireEvent } from "@testing-library/react";
-
 
 import Form from "components/Appointment/Form";
 
 afterEach(cleanup);
 
 describe("Form", () => {
-  const { getByPlaceholderText } = render(
-    <Form interviewers={interviewers} />
-  );
   const interviewers = [
     {
       id: 1,
@@ -26,39 +21,5 @@ describe("Form", () => {
 
   it("renders with initial student name", () => {
     expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
-  });
-  it("validates that the student name is not blank", () => {
-    /* 1. Create the mock onSave function */
-  
-    /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the student prop should be blank or undefined */
-  
-    /* 3. Click the save button */
-  
-    expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
-    expect(onSave).not.toHaveBeenCalled();
-  });
-  
-  it("validates that the interviewer cannot be null", () => {
-    /* 1. Create the mock onSave function */
-  
-    /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the interviewer prop should be null */
-  
-    /* 3. Click the save button */
-  
-    expect(getByText(/please select an interviewer/i)).toBeInTheDocument();
-    expect(onSave).not.toHaveBeenCalled();
-  });
-  
-  it("calls onSave function when the name and interviewer is defined", () => {
-    /* 1. Create the mock onSave function */
-  
-    /* 2. Render the Form with interviewers, name and the onSave mock function passed as an onSave prop */
-  
-    /* 3. Click the save button */
-  
-    expect(queryByText(/student name cannot be blank/i)).toBeNull();
-    expect(queryByText(/please select an interviewer/i)).toBeNull();
-    expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
   });
 });
