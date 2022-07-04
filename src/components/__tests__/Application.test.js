809985797a8853719be950fd4f1c9adae7aceca6
Bazil <king.bazil.khan@gmail.com>
2022-07-03 22:58:02 -0600

    Updates application and appointment testing

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,17 +1,8 @@
-/*
-  We are rendering `<Application />` down below, so we need React.createElement
-*/
 import React from "react";
 import ReactDOM from "react-dom";
 
-/*
-  We import our helper functions from the react-testing-library
-  The render function allows us to render Components
-*/
 import { render, cleanup, waitForElement, wait, fireEvent, queryByAltText, getByText, getByAltText, getAllByTestId, prettyDOM, getByPlaceholderText, queryByText, queryAllByAltText } from "@testing-library/react";
-/*
-  We import the component that we are testing
-*/
+
 import axios from 'axios';
 import Application from "components/Application";
 import Empty from "components/Appointment/Empty";
@@ -23,10 +14,6 @@ beforeEach(() => {
 afterEach(cleanup);
 
 axios.defaults.baseURL = "http://localhost:8001";
-
-/*
-  A test that renders a React Component
-*/
 
 describe("Application", () => {
   it("defaults to Monday and changes the schedule when a new day is selected", async () => {
--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,13 +1,34 @@
+import React from "react";
+
+import { render, cleanup } from "@testing-library/react";
+
+import Appointment from "components/Appointment";
+import Confirm from "components/Appointment/Confirm";
+import Empty from "components/Appointment/Empty";
+import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Status from "components/Appointment/Status";
+
+afterEach(cleanup);
+
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+];
+
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png"
+};
+
 describe("Appointment", () => {
   it("renders without crashing", () => {
     render(<Appointment />);
-  });
-
-  it("does something it is supposed to do", () => {
-    // ...
-  });
-
-  it("does something else it is supposed to do", () => {
-    // ...
   });
 });
