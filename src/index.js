e973af26ca792b13a2d2c9831f150dcc703d1e54
Bazil <king.bazil.khan@gmail.com>
2022-07-07 08:48:51 +0000

    Remove unused imports

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "index.scss";
-
 import Application from "components/Application";
-import DayListItem from "components/DayListItem";
 
 ReactDOM.render(<Application />, document.getElementById("root"));
